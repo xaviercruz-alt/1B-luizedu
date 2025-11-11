f38ada2e626ff3fa6d60f26d6f67d90cc6293f95
xaviercruz-alt <xavier.cruz@escola.pr.gov.br>
2025-11-11 08:30:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 const avanca = document.querySelectorAll(".btn-proximo")
 
 avanca.forEach(button => {
-    button.addEventlitener('click', function(){
+    button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
-        const proximopasso = 'passo-' + this.getattribute('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
-        document.getElementById(proximopasso).classList.add('ativo')
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
-    
-    
- 
-    
